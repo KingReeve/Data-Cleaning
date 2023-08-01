41e5df898bb40475e39ba3d47e6608fc5064d7eb
KingReeve <140770868+KingReeve@users.noreply.github.com>
2023-07-31 20:57:06 -0500

    Update MakeCSVfromTestData.py

--- MakeCSVfromTestData.py
+++ MakeCSVfromTestData.py
@@ -34,20 +34,21 @@ for i in dataFromFile:
         x = re.search(r'(?<=Test.: )(.*?)(?= ...:)', i, flags=re.IGNORECASE).group() + ' ' + re.search(r'Cmd: (.*?)(?= )', i, flags=re.IGNORECASE).group()  #this will be the keys for the dictionary, the test names
     except:
         continue #skip lines that don't contain the test data.
-    templist = i.partition("Cmd: ") # couldn't find elegant solution, gave up and made a temporary, size-3 tuple and used re.search because it will only find the first occurrence.  The data comes after actual, and the important data is after the Cmd value.
+    templist = i.partition("Cmd: ") #made a temporary partition and used re.search because it will only find the first occurrence.  The data comes after actual, and the important data is after the Cmd value.
     y = re.search(r'(?<=Actual: )(.*?)(?= )', templist[2]).group() #this will be the values
     dataDict[x].append(y)
 
 if not dataDict:
-    raise SystemExit("File contained no appropriate data.") #dictionaries are false if empty.
+    raise SystemExit("File contained no appropriate data.") #dictionaries are falsy.
 
 ColumnLabel = DAUname + ' ' + Platformname
 LabelDict = {'Test#' : ColumnLabel}
 pdReadyDict = LabelDict #getting ready to update, could probably just use LabelDict.
 for d in dataDict: pdReadyDict.update(dataDict) #just appends the data after the "header"
-flatpdreadydict = flattenDictionary(pdReadyDict) #runs the flattening code from earlier.  The data comes with many duplicates without data, but which may    have data in the future, so they can't be deleted.
+flatpdreadydict = flattenDictionary(pdReadyDict) #runs the flattening code from earlier.  The data comes with many duplicates without data, but which may have data in the future, so they can't be deleted.
 
 #PandasFrame = pandas.DataFrame.from_dict(dataDict)  Dataframe won't take varying lengths like this
+#could actually probably start using dataframe.from_dict() again, since I'm now flattening the data.
 PandasFrame = pandas.DataFrame({k: pandas.Series(v) for k,v in flatpdreadydict.items()}) #taking key/values and putting them into an array, and mushing them together
 excelReadyPandasFrame = PandasFrame.transpose(copy=True) #want rows to be new data, rather than columns. Eventually, the columns will be merged into one     file to do some data analysis.
 excelReadyPandasFrame.to_csv(dataoutput)
